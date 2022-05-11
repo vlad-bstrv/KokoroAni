72982eeaf34840e75753b36cc77f7eec15a37617
vlad-bstrv <keocera@icloud.com>
2022-05-11 22:15:24 +0300

    add koin and baseFragment

---
+++ app/src/main/java/com/vladbstrv/kokoroani/di/viewModelModule.kt
@@ -0,0 +1,9 @@
+package com.vladbstrv.kokoroani.di
+
+import com.vladbstrv.kokoroani.ui.main.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { MainViewModel() }
+}
--- app/src/main/java/com/vladbstrv/kokoroani/ui/MainActivity.kt
+++ app/src/main/java/com/vladbstrv/kokoroani/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vladbstrv.kokoroani
+package com.vladbstrv.kokoroani.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vladbstrv.kokoroani.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
